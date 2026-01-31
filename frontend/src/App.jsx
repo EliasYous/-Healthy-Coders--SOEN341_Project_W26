8fe112c257e95a6d4b4e341612abdfb363320190
Elias Youssef <e_yf@outlook.com>
2026-01-31 18:17:24 -0500

    added axios and react-router-dom dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <BrowserRouter>
-
+      <h1>Healthy Coders</h1>
     </BrowserRouter>
   )
 }
