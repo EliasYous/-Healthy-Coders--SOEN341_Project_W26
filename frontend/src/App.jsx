da3844bb34fe26dee63fefbd06781c3f985aeff9
Dipita Sinha <144612228+DipiAreum04@users.noreply.github.com>
2026-02-06 03:22:33 -0500

    resolved task.07 (Design Login UI)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,18 @@ import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import './App.css'
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div>
       <h1>Testing</h1>
       <BrowserRouter>
-        <Register />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
       </BrowserRouter>
     </div>
   )
