f8827aa326fe98e9d5a5b2c72c379881d1b9e493
Elias Youssef <e_yf@outlook.com>
2026-02-07 17:42:39 -0500

    updated server config resized brand logo in navbar

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const morgan = require('morgan');
 
-const initDatabase = require('./config/dbInit');
+const initDatabase = require('./config/dbinit');
 const authRoutes = require('./routes/auth');
 const profileRoutes = require('./routes/profile');
 
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(morgan('dev'));
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173'],
   credentials: true,
 }));
 app.use(express.json());
